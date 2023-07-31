03b7dcdb289d2eb69593f965c1cece5b6f9e2bd2
AntonelloStefania <antonelloste94@gmail.com>
2023-07-31 15:49:05 +0200

    creati tutti i files necessari, milestone 1 completata

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+//Route::php artisan make:model NomeModello -rcmsresource('comics', ComicsController::class);
