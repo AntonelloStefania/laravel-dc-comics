7df02951ea4e10b38903e685a148db428d355721
AntonelloStefania <antonelloste94@gmail.com>
2023-07-31 22:58:24 +0200

    terminata sezione show

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -9,13 +9,15 @@
                    
                    @foreach($comics as $comic)
                    
-                        <div class="card col-3 m-2">
+                        <div class="card col-3 m-2 d-flex flex-column justify-content-between">
                             <img src="{{$comic->thumb}}" class="h-75 card-img-top img-fluid" alt="{{$comic->thumb2}}">
                             <div class="p-3 card-text text-center">
                                 <h4>{{$comic->title}}</h4>
                                 <h6 class="py-3">{{$comic->series}}</h6>
                             </div>
-                            <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-sm btn-warning w-50">MORE</a>
+                            <div class="d-flex justify-content-center pb-2">
+                                <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-sm btn-warning w-50">MORE</a>
+                            </div>
                         </div>
                    @endforeach
                 </div>
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,7 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-6 my-5 text-center">
+                <img src="{{$comic->thumb}}" alt="{{$comic->thumb2}}">
+                <div class="py-4">
+                    <h3 class="text-center">{{$comic->title}}</h3>
+                    <h5>{{$comic->series}}</h5>
+                </div>
+                <div class="py-3 text-center">
+                    <h6><strong>Type:</strong> {{$comic->type}}</h6>
+                    <span><strong>Release Date:</strong> {{$comic->sale_date}}</span>
+                    <div class="mt-5">
+                        <h4>Plot:</h4>
+                        <p >{{$comic->description}}</p>
+                    </div>
+                    <h6><strong>Price:</strong> {{$comic->price}}</h6>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 @endsection
